a4118a9c2b27dcf7757c8e3f3967e4c34ec45d83
yosrimhamdi <bavary1515@gmail.com>
2021-04-23 19:00:13 +0100

    using mailtramp.

--- script.php
+++ script.php
@@ -81,12 +81,12 @@ try {
   //Server settings
   $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
   $mail->isSMTP();                                            //Send using SMTP
-  $mail->Host       = 'smtp.mandrillapp.com';                     //Set the SMTP server to send through
+  $mail->Host       = 'smtp.mailtrap.io';                     //Set the SMTP server to send through
   $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-  $mail->Username   = 'yosri';                     //SMTP username
-  $mail->Password   = 'xXPuZzGURNY58UfDVoVUfQ';                               //SMTP password
+  $mail->Username   = 'd310d179e8b8d3';                     //SMTP username
+  $mail->Password   = '2fb18b74a7b93e';                               //SMTP password
   $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-  $mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+  $mail->Port       = 2525;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
   //Recipients
   $mail->setFrom('from@example.com', 'Mailer');
@@ -97,9 +97,8 @@ try {
 
   //Content
   $mail->isHTML(true);                                  //Set email format to HTML
-  $mail->Subject = 'Here is the subject';
-  $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-  $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+  $mail->Subject = 'auth cridentials p12';
+  $mail->Body    = 'your p12 file is shown below';
 
   $mail->send();
   echo 'Message has been sent';
